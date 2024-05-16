613950d144b08642d0da99a2a4141e8de156b626
kaoniqiwa <1061886912@qq.com>
2024-05-16 10:26:58 +0800

    搜索功能完成

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -4,15 +4,26 @@
 		<uni-search-bar placeholder="请输入搜索内容" @input="input" :radius="100" cancelButton="none"
 			:focus="true"></uni-search-bar>
 
-
 		<!-- 搜索建议列表 -->
-		<view class="sugg-list">
+		<view class="sugg-list" v-if="searchResult.length">
 			<view class="sugg-item" v-for="(item, i) in searchResult" :key="i" @click="gotoDetail(item.goods_id)">
 				<view class="goods-name">{{item.goods_name}}</view>
 				<uni-icons type="arrowright" size="16"></uni-icons>
 			</view>
 		</view>
 
+		<!-- 搜索历史 -->
+		<view class="history-box" v-else>
+			<!-- 标题区域 -->
+			<view class="history-title">
+				<text>搜索历史</text>
+				<uni-icons type="trash" size="17" @click="clean"></uni-icons>
+			</view>
+			<!-- 列表区域 -->
+			<view class="history-list">
+				<uni-tag :text="item" v-for="(item, i) in historys" :key="i" @click="gotoGoodsList(item)"></uni-tag>
+			</view>
+		</view>
 
 	</view>
 
@@ -20,6 +31,8 @@
 
 <script setup>
 	import {
+		computed,
+		onBeforeMount,
 		ref
 	} from 'vue';
 	import {
@@ -53,9 +66,52 @@
 			return
 		}
 		searchResult.value = data.message
+		saveSearchHistory(query)
+	}
+	/**点击跳转详情*/
+	const gotoDetail = (goods_id) => {
+		uni.navigateTo({
+			url: `/subpkg/goods_detail/goods_detail?goods_id=${goods_id}`
+		})
 	}
 
-	const gotoDetail = () => {}
+	/**搜索历史*/
+	const historyList = ref([])
+	const historys = computed(() => Array.from(historyList.value).reverse())
+	const saveSearchHistory = (query) => {
+
+		const index = historyList.value.findIndex((v) => v === query)
+		if (index !== -1) {
+			historyList.value.splice(index, 1)
+		}
+		historyList.value.push(query)
+
+		/**数据持久化 wx.setStorageSync*/
+		uni.setStorageSync('search_history', JSON.stringify(historyList.value))
+	}
+	const getHistoryList = () => {
+		const searchHistory = uni.getStorageSync('search_history');
+		if (searchHistory) {
+			historyList.value = JSON.parse(searchHistory)
+		}
+	}
+
+	/**清空历史数据*/
+	const clean = () => {
+		historyList.value = [];
+		uni.removeStorageSync('search_history')
+	}
+
+	const gotoGoodsList = (query) => {
+		uni.navigateTo({
+			url: `/subpkg/goods_list/goods_list?query=${query}`
+		})
+	}
+	/**服务端不会触发该钩子*/
+	onBeforeMount(() => {
+		getHistoryList()
+
+	})
 </script>
 
 <style lang="scss" scoped>
@@ -85,9 +141,42 @@
 		}
 	}
 
+	.history-box {
+		padding: 0 5px;
 
-	::v-deep .uni-searchbar {
-		background-color: #c00000;
+		.history-title {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			height: 40px;
+			font-size: 13px;
+			border-bottom: 1px solid #efefef;
+		}
 
+		.history-list {
+			display: flex;
+			flex-wrap: wrap;
+			margin-top: 5px;
+
+
+		}
+	}
+
+
+
+
+	::v-deep {
+		.uni-searchbar {
+			background-color: #c00000;
+		}
+
+		.uni-tag {
+			margin-right: 5px;
+			background-color: #8f939c6b;
+			border: none;
+			color: #000000a8;
+			font-weight: bold;
+			font-size: 14px;
+		}
 	}
 </style>
